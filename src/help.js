d1396feb14a75389d0676bb94e1ba716b6ff6384
falconbot3 <81450418+falconbot3@users.noreply.github.com>
2021-03-27 17:11:03 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,171 @@
+<══════════════════════>
+      
+                            🤖𝙵𝙰𝙻𝙲𝙾𝙽 𝙱𝙾𝚃🤖 <══════════════════════>
+
+__█████████  ● ᏴϴͲ●ᎷᎬΝႮ●         
+__█▄█████▄█   Author    : ❦𝙵𝙰𝙻𝙲𝙾𝙽 𝙱𝙾𝚃❦
+__█▼▼▼▼▼█ 
+_██ᏴϴͲ Ꮩ1.2██▌ 
+__█▲▲▲▲▲█ 
+__█████████ 
+____██_____██
+
+┌─────────❶.❷────────┐
+└─────────❶.❷────────┘
+╔══════════════════════
+║ ᏟϴᎷᎪΝᎠϴՏ ᏞᏆՏͲᎪᎠϴՏ
+╠══════════════════════
+
+     𝐸𝑋𝐶𝐿𝑈𝑆𝐼𝑉𝑂𝑆 𝑃𝐴𝑅𝐴 𝐴𝐷𝑀𝐼𝑁𝑆
+
+║├> .${prefix}banir
+
+║├> .${prefix}add
+
+║├> .${prefix}rebaixar
+
+║├> .${prefix}promover
+
+║├> .${prefix}setfoto
+
+║├> .${prefix}level
+
+║├> .${prefix}leveling [on/off]
+
+║├> .${prefix}setname [texto]
+
+║├> .${prefix}marcar
+
+║├> .${prefix}marcar2
+
+║├> .${prefix}marcar3
+
+║├> .${prefix}bemvindo [1/0]
+
+║├> .${prefix}grupoinfo 
+
+║├> .${prefix}bomdia
+
+║├> .${prefix}boatarde
+
+║├> .${prefix}boanoite
+
+║├> .${prefix}setdesc
+
+║├> .${prefix}bug
+
+║├> .${prefix}antilink [1/0]
+
+║├> .${prefix}setnomebot
+
+║├> .${prefix}meme
+
+║├> .${prefix}linkgp
+
+
+••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+
+             𝑆𝑂𝑀𝐸𝑁𝑇𝐸 𝑃𝐴𝑅𝐴 𝑂 𝐷𝑂𝑁𝑂 𝐷𝑂 𝐵𝑂𝑇
+
+║├> .${prefix}desligar
+
+║├> .${prefix}bc
+
+║├> .${prefix}limpar
+
+║├> .${prefix}bloquear
+
+
+°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+
+                    𝑃𝑅𝐸𝑀𝐼𝑈𝑀
+
+║├> .${prefix}chentai
+
+║├> .${prefix}pack
+
+║├> .${prefix}destrava
+
+║├> .${prefix}gpessoa
+
+║├> .${prefix}gbin
+
+║├> .${prefix}gcpf
+
+║├> .${prefix}delete
+
+║├> .${prefix}qrcode
+
+║├> .${prefix}daftarvip
+
+║├> .${prefix}dado
+
+
+°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+
+                 𝐴𝑉𝐴𝑁𝐶̧𝐴𝐷𝑂 
+
+║├> .${prefix}simih 1 [on]
+
+║├> .${prefix}simih 0 [off]
+
+║├> .${prefix}simi [sua mensagem]
+
+
+°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+
+              𝐼𝑀𝐴𝐺𝐸𝑁𝑆 𝐴𝐿𝐸𝐴𝑇𝑂́𝑅𝐼𝐴𝑆 
+
+║├> .${prefix}meme
+
+║├> .${prefix}lofi
+
+║├> .${prefix}reislin
+
+║├> .${prefix}nsfwloli1
+
+║├> .${prefix}hinata
+
+║├> .${prefix}sasuke
+
+║├> .${prefix}naruto
+
+║├> .${prefix}rize
+
+║├> .${prefix}porno
+
+║├> .${prefix}hentai
+
+║├> .${prefix}loli [off]
+
+║├> .${prefix}loli1
+
+
+°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+
+               𝐷𝐼𝑉𝐸𝑅𝑇𝐼𝐷𝑂𝑆
+
+║├> .${prefix}figu [para fazer figurinhas]
+
+║├> .${prefix}toimg
+
+║├> .${prefix}tts
+
+║├> .${prefix}nsfwloli [off]
+
+║├> .${prefix}lolih [on]
+
+║├> .${prefix}wame
+
+║├> .${prefix}play [nome da música]
+
+║├> .${prefix}spamcall
+
+
+
+
+🔰𝐄𝐒𝐏𝐄𝐑𝐎 𝐐𝐔𝐄 𝐆𝐎𝐒𝐓𝐄𝐌 𝐃𝐎 𝐁𝐎𝐓🔰
+
+             👇🏼𝐌𝐄𝐔 𝐃𝐎𝐍𝐎👇🏼
+    
+           Wa.me/+5521993471668
